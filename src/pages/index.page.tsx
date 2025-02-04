11d3cf8dacacd470d556f05a3bc24d63eb7bbef0
doannc02 <sillver47108@gmail.com>
2025-02-05 05:02:50 +0700

    update import dashboard

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,21 +1,28 @@
 import { BasicLayout } from '@/components/layouts/WrapLayout/BasicLayout'
 import { Meta } from '@/components/meta'
-import Dashboard from '@/components/templates/Customer/Dashboard'
 import { HttpResponse } from '@/lib/api'
 import { combineGssp } from '@/lib/next/gssp/combineGssp'
-import { authGssp } from '@/lib/next/gssp/middleware/authGssp'
 import { NextPageWithLayout } from '@/lib/next/types'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import dynamic from 'next/dynamic'
 
 type Props = HttpResponse<any>
 
-const Page: NextPageWithLayout<Props> = () => <Dashboard />
+// Import Dashboard with dynamic to disable SSR
+const DashboardUser = dynamic(
+  () => import('@/components/templates/Customer/Dashboard'),
+  {
+    ssr: false, // Disable SSR for Dashboard component
+  }
+)
+
+const Page: NextPageWithLayout<Props> = () => <DashboardUser />
 
 Page.getLayout = BasicLayout
-Page.getMeta = Meta(() => ({ title: 'Dashboard Exam Process Manage Service' }))
+Page.getMeta = Meta(() => ({ title: 'Order food' }))
 
 export const getServerSideProps = combineGssp<any>(
-  authGssp(),
+  // authGssp(),
   async ({ locale = 'vn' }) => ({
     props: {
       ...(await serverSideTranslations(locale, ['common'])),
